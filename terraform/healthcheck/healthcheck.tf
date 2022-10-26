8272fc3c3582df43895607fb131281c2a578f058
Oleksandr Rudenko <oleksandr.r@quali.com>
2022-10-26 17:26:16 +0300

    remove tf timeout

--- terraform/healthcheck/healthcheck.tf
+++ terraform/healthcheck/healthcheck.tf
@@ -3,7 +3,7 @@
 resource "null_resource" "healthcheck" {
   provisioner "local-exec" {
     on_failure  = fail
-    command     = "chmod +x ./scripts/healthcheck.sh && timeout 10m ./scripts/healthcheck.sh"
+    command     = "chmod +x ./scripts/healthcheck.sh && ./scripts/healthcheck.sh"
     interpreter = ["/bin/bash", "-c"]
 
     environment = {
@@ -12,3 +12,5 @@ resource "null_resource" "healthcheck" {
     }
   }
 }
+
+# timeout 10m 
